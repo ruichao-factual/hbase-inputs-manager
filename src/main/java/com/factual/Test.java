ea5547bcdf1eff5650253f31c85708f4e4ef1ebc
ruichao-factual <ruichao@factual.com>
2014-07-31 18:05:43 +0800

    add a small test example

--- src/main/java/com/factual/Test.java
+++ src/main/java/com/factual/Test.java
@@ -4,30 +4,16 @@ import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 
-import org.apache.hadoop.hbase.HColumnDescriptor;
-
-import org.apache.hadoop.hbase.HTableDescriptor;
-
-import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.client.HTable;
 
 public class Test {
   public static void main(String[] args) throws IOException
   {
-    Configuration hc = HBaseConfiguration.create();
-
-    HTableDescriptor ht = new HTableDescriptor("User");
-
-    ht.addFamily( new HColumnDescriptor("Id"));
-
-    ht.addFamily( new HColumnDescriptor("Name"));
-
-    System.out.println( "connecting" );
-
-    HBaseAdmin hba = new HBaseAdmin( hc );
-
-    System.out.println( "Creating Table" );
-
-    hba.createTable( ht );
+    Configuration conf = HBaseConfiguration.create();
+    HTable hTable = new HTable(conf, "-ROOT-");
+    System.out.println("Table is:" + Bytes.toString(hTable.getTableName()));
+    hTable.close();
 
     System.out.println("Done......");
   }
