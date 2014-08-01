e6ab609d49382aa99b24dfedf8215dea331caeab
ruichao-factual <ruichao@factual.com>
2014-08-01 16:53:24 +0800

    remove a few useless lines

--- src/main/java/com/factual/HbaseInputsManager.java
+++ src/main/java/com/factual/HbaseInputsManager.java
@@ -109,9 +109,6 @@ public class HbaseInputsManager {
       e.printStackTrace();
     }
 
-    String rawInput = "{\"md5\":\"006c669f4aad31d1b4ace612ddca716e\",\"payload\":{\"name\":\"香港丽思卡尔顿酒店\",\"address\":\"九龙油尖旺区尖沙咀柯士甸道西1号环球贸易广场102-118楼\",\"tel\":\"852 2263 2263\",\"website\":\"http://www.ritzcarlton.com/zh-cn/Properties/HongKong/Default.htm\",\"category_labels\":[\"hotel\"],\"latitude\":\"22.30348018828219\",\"longitude\":\"114.16021227836609\",\"foreign_id\":\"297\",\"country\":\"hk\",\"hours\":null},\"inputMeta\":{\"origin\":\"SEED\",\"sourceUrl\":\"http://www.160.com.hk/hotel/bencandy-htm-city_id-1-fid-24-id-297.html\",\"notes\":{\"_rs_id\":1367,\"_ds_id\":28,\"_rs_v\":1378268958},\"targetViewAlias\":\"places-hk\"},\"processingState\":\"UNPROCESSED\",\"inputDate\":1378290224000}\t";
-    Htable htable = new Htable("hk");
-    htable.put(rawInput);
   }
 
   public void upload(String inputPath, String tableName, String inputType) throws Exception{
