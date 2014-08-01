1c74deea616265d91ad9f1b41287a78ac6de2f3e
ruichao-factual <ruichao@factual.com>
2014-08-01 09:56:57 +0800

    a simple framework

--- src/main/java/com/factual/HbaseInputsManager.java
+++ src/main/java/com/factual/HbaseInputsManager.java
@@ -12,16 +12,27 @@ import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.util.Bytes;
 
+import org.apache.commons.cli.*;
+
 
 // Class that has nothing but a main.
 // Does a Put, Get and a Scan against an hbase table.
-public class Test {
+public class HbaseInputsManager {
   public static void main(String[] args) throws IOException {
+    Option help = new Option("h", "prints this message");
+    OptionBuilder.withArgName("command");
+    OptionBuilder.hasArg();
+    OptionBuilder.withDescription("command [put, search, update]");
+    Option command = OptionBuilder.create("c");
+
+
+
+
     Configuration conf = HBaseConfiguration.create();
     HTable hTable = new HTable(conf, "HBaseSamples");
     Put put1 = new Put(Bytes.toBytes("row1"));
-    put1.add(Bytes.toBytes("test"), Bytes.toBytes("col1"), Bytes.toBytes("value1"));
-    put1.add(Bytes.toBytes("test"), Bytes.toBytes("col2"), Bytes.toBytes("value2"));
+    put1.add(Bytes.toBytes("test"), Bytes.toBytes("col3"), Bytes.toBytes("value4"));
+    put1.add(Bytes.toBytes("test"), Bytes.toBytes("col5"), Bytes.toBytes("value6"));
     hTable.put(put1);
     hTable.close();
   }
